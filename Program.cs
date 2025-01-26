a75f1e51b3639f011be9d106a1a1e64b0a618e3f
Zohair <zohair1991@gmail.com>
2025-01-26 18:30:38 +0100

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BuildWithSyed_Solid_Principles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
