fdfbc68a1efbfbadf58fa05543e97beee70cca07
Zohair <zohair1991@gmail.com>
2025-01-26 18:58:03 +0100

    SRP

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace BuildWithSyed_Solid_Principles
+{
+    /* !!!!!!!!!!!!!!!!!!!!!!!!!! SINGLE RESPONSIBILITY PRINCIPLE IS Refactored HERE !!!!!!!!!!!!!!!!!!!!!!!!!! 
+                 The SRP states that a class should have only one reason to change.In this case:
+                 If the logic for toy creation changes, the class will need to change.
+                 If the packing process changes, the class will also need to change.
+                 If the delivery method changes, the class will again need to change. 
+      */
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            /*  SINGLE RESPONSIBILITY PRINCIPLE */
+            //ToyMaker toyMaker = new ToyMaker();
+            //ToyPacker toyPacker = new ToyPacker();
+            //ToyDeliveryService toyDelivery = new ToyDeliveryService();
+
+            //toyMaker.MakeAToy();
+            //toyPacker.PackMyToy();
+            //toyDelivery.DeliverMyToy();
+
+            /*  SINGLE RESPONSIBILITY PRINCIPLE */
+        }
+    }
+}
--- SingleResponsibilityPrinciple.cs
+++ SingleResponsibilityPrinciple.cs
@@ -2,9 +2,9 @@
 {
     /* !!!!!!!!!!!!!!!!!!!!!!!!!! SINGLE RESPONSIBILITY PRINCIPLE IS Refactored HERE !!!!!!!!!!!!!!!!!!!!!!!!!! 
                  The SRP states that a class should have only one reason to change.In this case:
-                 If the logic for toy creation changes, the class will need to change.
-                 If the packing process changes, the class will also need to change.
-                 If the delivery method changes, the class will again need to change. 
+                 If the logic for toy creation changes, the class ToyMaker will need to change.
+                 If the packing process changes, the class will ToyPacker need to change.
+                 If the delivery method changes, the class will ToyDeliveryService need to change. 
       */
     public class ToyMaker
     {
@@ -36,21 +36,6 @@
         public void DeliverMyToyToOffice()
         {
             Console.WriteLine("Toy has been delivered");
-        }
-    }
-
-    public class ProgramViolated
-    {
-        static void Main(string[] args)
-        {
-            //We have a separated class created, for each type of service
-            ToyMaker toyMaker = new ToyMaker();
-            ToyPacker toyPacker = new ToyPacker();
-            ToyDeliveryService toyDelivery = new ToyDeliveryService();
-
-            toyMaker.MakeAToy();
-            toyPacker.PackMyToy();
-            toyDelivery.DeliverMyToy();
         }
     }
 }
